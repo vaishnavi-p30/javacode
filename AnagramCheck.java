2062608097f3b1ad5cee09b61999e7990f90f50b
vaishnavi-p30 <vaishnaviperiyasamy2005@gmail.com>
2025-04-18 16:35:37 +0530

    first commit

---
+++ AnagramCheck.java
@@ -0,0 +1,34 @@
+import java.util.Arrays;
+
+public class AnagramCheck {
+    public static void main(String[] args) {
+        String str1 = "listen";
+        String str2 = "silent";
+
+        if (isAnagram(str1, str2)) {
+            System.out.println("Yes, they are anagrams.");
+        } else {
+            System.out.println("No, they are not anagrams.");
+        }
+    }
+
+    static boolean isAnagram(String s1, String s2) {
+        // Remove spaces and convert to lowercase
+        s1 = s1.replaceAll("\\s", "").toLowerCase();
+        s2 = s2.replaceAll("\\s", "").toLowerCase();
+
+        // Check lengths
+        if (s1.length() != s2.length()) {
+            return false;
+        }
+
+        // Convert to char array, sort, and compare
+        char[] arr1 = s1.toCharArray();
+        char[] arr2 = s2.toCharArray();
+
+        Arrays.sort(arr1);
+        Arrays.sort(arr2);
+
+        return Arrays.equals(arr1, arr2);
+    }
+}
